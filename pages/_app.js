66d0689272f4d81b6bffe8cc73c7d62183bf3ebc
Ana Hordieieva <gordeevaisgreat@gmail.com>
2023-09-09 09:53:26 +0300

    add notifications

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,19 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Layout from "../components/layout/layout";
+import "../styles/globals.css";
+import Notification from "../components/ui/notification";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
         <title>Next Events</title>
-        <meta name='description' content='NextJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name="description" content="NextJS Events" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification title="test" message="test" status="pending" />
     </Layout>
   );
 }
